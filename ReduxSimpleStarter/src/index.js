072a62975d6776d0c624beac725175a1372bbc8e
Glen Pinner <glen.pinner@gmail.com>
2017-04-11 15:27:12 +0100

    Section 1: An Intro to React.

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/SearchBar';
+
+const API_KEY = 'AIzaSyDFg0Rsp0mDnoYztmcxwdDvqJ3J74TydsM';
+
+
+// Create a new component. This component should produce some HTML.
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
+
+// Take this component's generated HTML and put it on the page (in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
